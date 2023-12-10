9dec7024deeb2c415ebc0d54cb2ed080f9ab104d
Asus <Asus@DESKTOP-LUBP48L>
2023-12-10 15:25:15 +0600

    Добавьте файлы проекта.

---
+++ Module09HM/Employee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module09HM
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+        public double BaseSalary { get; set; }
+
+        public Employee(string name, double baseSalary)
+        {
+            Name = name;
+            BaseSalary = baseSalary;
+        }
+        public virtual double CalculateAnnualSalary()
+        {
+            return BaseSalary * 12;
+        }
+    }
+
+}
+
---
+++ Module09HM/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module09HM
+{
+    public class Program
+    {
+        static void Main()
+        {
+            Manager manager = new Manager("iman", 190000, 10000);
+            double annualSalary = manager.CalculateAnnualSalary();
+
+            Console.WriteLine($"{manager.Name} годовой оклад: ${annualSalary}");
+            Console.ReadKey();
+        }
+    }
+}
